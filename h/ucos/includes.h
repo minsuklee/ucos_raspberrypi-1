acebea63c26dc43e705ec469a55f658a0b9edcb7
Li Tao <watass@126.com>
2013-05-24 14:01:47 +0800

    1st submit at 2013/5/24

---
+++ h/ucos/includes.h
@@ -0,0 +1,7 @@
+#include    "ucos/os_cpu.h"
+#include    "ucos/os_cfg.h"
+#include    "ucos/ucos_ii.h"
+
+extern void *memcpy(void *destination,const void *source,unsigned int size);
+
+extern void *memset(void * m,int c,unsigned int size);
